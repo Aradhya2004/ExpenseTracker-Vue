f427b5ff253993b4846601a08411d0202e8cf890
Aradhya2004 <srivastavaaradhya9@gmail.com>
2025-11-20 14:27:18 +0530

    Dynamic balances, income & expenses

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,45 @@
   import Header from './components/Header.vue';
   import IncomeExpense from './components/IncomeExpense.vue';
   import TransactionList from './components/TransactionList.vue';
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
 
   const transactions = ref([
         {id: 1, text: 'Salary', amount: +7292},
         {id: 2, text: 'Stipend', amount: +6000},
         {id: 3, text: 'Rent', amount: -5500 },
         {id: 4, text: 'Party', amount: -900}
-    ])
+  ])
+
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+  })
+
+   const income = computed(() => {
+    return transactions.value
+      .filter((transaction) => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0)
+      .toFixed(2);
+   })
+
+   const expenses = computed(() => {
+     return transactions.value
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0)
+      .toFixed(2);
+   })
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpense />
+    <Balance :total = "total" />
+    <IncomeExpense :income= "income" :expenses= "expenses" />
     <TransactionList :transactions = "transactions" />
     <AddTransaction />
   </div>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,4 +1,15 @@
 <template>
     <h4>Your Balance</h4>
-    <h1>Rs 0.00</h1>
+    <h1>Rs {{ total }}</h1>
 </template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        total: {
+            type: Number,
+            required: true,
+        },
+    });
+</script>
